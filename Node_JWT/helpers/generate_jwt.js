f31b689e8e4dc464ef16e995ad037917052eae5b
faranzabe <faranzabe@gmail.com>
2023-01-13 10:14:26 +0100

    JWT

--- Node_JWT/helpers/generate_jwt.js
+++ Node_JWT/helpers/generate_jwt.js
@@ -5,7 +5,7 @@ const generarJWT = (uid = '') => {
     
     //En el token podemos hacer que viaje (en el payload) el id de ese usuario. No supone un gran fallo de seguridad y nos permite sacar la información del mismo en los middleware.
     console.log("UID:" + uid)
-    var token = jwt.sign({ uid }, process.env.SECRETORPRIVATEKEY, {
+    let token = jwt.sign({ uid }, process.env.SECRETORPRIVATEKEY, {
         expiresIn: '4h' // 24 hours
       });
     return token;
